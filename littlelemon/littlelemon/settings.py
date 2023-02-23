01e5d7c661a7afba16c289400f09e92682bfab22
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-23 16:56:38 +0200

    set templates dir

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -35,12 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # APPs
-    'restaurant',
     # 3-thd party apps
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    # APPs
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,9 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
