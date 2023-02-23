dc2d3c3ba80d8b79eeb9f3b9a0985049fec7f168
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-23 11:58:46 +0200

    Add namespaces

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -6,7 +6,7 @@ booking_routers = DefaultRouter()
 booking_routers.register(r'tables', BookingViewSet)
 
 urlpatterns = [
-    path('menu/', MenuItemView.as_view()),
-    path('menu/<int:pk>/', SingleMenuItemView.as_view()),
-    path('bookings/', include(booking_routers.urls)),
+    path('menu/', MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>/', SingleMenuItemView.as_view(), name='menu'),
+    path('bookings/', include(booking_routers.urls), name='bookings'),
 ]
