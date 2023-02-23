51e348f45873808f1a2e5e9075f7975c45a6dd37
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-23 19:22:43 +0200

    transfer api-token-auth endpoint

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import MenuItemView, SingleMenuItemView, BookingViewSet, index
 
 booking_routers = DefaultRouter()
@@ -10,4 +11,5 @@ urlpatterns = [
     path('menu/', MenuItemView.as_view(), name='menu'),
     path('menu/<int:pk>/', SingleMenuItemView.as_view(), name='menu'),
     path('bookings/', include(booking_routers.urls), name='bookings'),
+    path('api-token-auth/', obtain_auth_token),
 ]
