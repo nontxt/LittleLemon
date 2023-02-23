d3d7de521567cbba14b87904d5ebff5eac6cc933
Viktor Nizhenets <nizhenets.nvv@gmail.com>
2023-02-23 16:11:43 +0200

    Add index rout

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MenuItemView, SingleMenuItemView, BookingViewSet
+from .views import MenuItemView, SingleMenuItemView, BookingViewSet, index
 
 booking_routers = DefaultRouter()
 booking_routers.register(r'tables', BookingViewSet)
 
 urlpatterns = [
+    path('', index, name='index'),
     path('menu/', MenuItemView.as_view(), name='menu'),
     path('menu/<int:pk>/', SingleMenuItemView.as_view(), name='menu'),
     path('bookings/', include(booking_routers.urls), name='bookings'),
